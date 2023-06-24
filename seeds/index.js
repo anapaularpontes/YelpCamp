9bf48c994848d34b9332f59dc8a25aee1f6cc233
Ana Pontes <anaruizpon@gmail.com>
2023-06-24 11:37:54 -0700

    Add cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -31,7 +31,7 @@ const seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [-113.1331, 47.0202]
+        coordinates: [cities[random1000].longitude, cities[random1000].latitude]
       },
       images: [
         {
@@ -42,7 +42,6 @@ const seedDB = async () => {
             url: 'https://res.cloudinary.com/dxbjyq9hm/image/upload/v1687392630/YelpCamp/adb4pdyebkl6pcjbm9sv.jpg',
             filename: 'YelpCamp/adb4pdyebkl6pcjbm9sv'
         }
-
       ]
     });
     await camp.save();
